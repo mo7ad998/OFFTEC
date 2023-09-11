590c4a508fb20d4061a85ecf4af94303f7261420
mo7ad998 <mo7ad.ayyad@gmail.com>
2023-09-11 20:42:22 +0300

    Build OFFTEC Task

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Client.Models;
+using Newtonsoft.Json;
+using WeatherMVC.Services;
+using IdentityModel.Client;
+
+namespace Client.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ITokenService _tokenService;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ITokenService tokenService,  ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        _tokenService = tokenService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Weather()
+    {
+        var data = new List<WeatherData>();
+        using (var client = new HttpClient())
+        {
+            var tokenResponse = await _tokenService.GetToken(scope: "api.read");
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var result = client.GetAsync(requestUri: "https://localhost:7116/WeatherForecast").Result;
+
+            if(result.IsSuccessStatusCode)
+            {
+                var model = result.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<List<WeatherData>>(model);
+
+                return View(data);
+            }
+            else
+            {
+                throw new Exception("Unable to get content");
+            }
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                    {
+                      Name = "role",
+                      UserClaims = new List<string> {"role"}
+                    }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api.read"),
+                new ApiScope("api.write")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+        {
+            new Client
+            {
+                ClientId = "OFFTEC",
+                ClientName = "OFFTEC Credentials Client",
+                ClientSecrets = { new Secret("OFFTEC".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"api.read", "api.write"}
+            }
+        };
+
+        public static IEnumerable<ApiResource> ApiResources => new[]
+        {
+          new ApiResource("api")
+            {
+              Scopes = new List<string> {"api.read", "api.write"},
+              ApiSecrets = new List<Secret> {new Secret("OFFTEC".Sha256())},
+              UserClaims = new List<string> {"role"}
+            }
+        };
+    }
+}
